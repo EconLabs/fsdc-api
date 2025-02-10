12d75466eb3105d20e06e4be390e9f89a8bdebbc
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-02-10 18:13:56 -0400

    added submodule

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.fsdc_calories.src.data_process import DataCal
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/charts/price_ranking")
+async def price_charts():
+    DataCal().gen_graphs().to_html()
